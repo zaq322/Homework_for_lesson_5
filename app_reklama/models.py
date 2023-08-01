0678e4291c6133172b252463308eccbe184d69bf
zaq322 <pkitalet@gmail.com>
2023-08-01 14:06:29 +0300

    Hard

--- app_reklama/models.py
+++ app_reklama/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Reklama(models.Model):
+    class Meta:
+        db_table = 'reklamka'
+
     title = models.CharField("заголовок", max_length=128)
     text = models.TextField("текст")
     price = models.FloatField("цена")
-    user = models.CharField("пользователь", max_length=126) # Просто имя
+    user = models.CharField("пользователь", max_length=126)  # Просто имя
     date = models.DateField("дата", auto_now_add=True)
+    def __str__(self):
+        return f"{self.text}, {self.price}, {self.title}"
+
