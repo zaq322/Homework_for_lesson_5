17b25b0d3d5fa9509b1428022cb367489b4a22c1
zaq322 <pkitalet@gmail.com>
2023-08-19 15:00:36 +0300

    Hard

--- reklama/urls.py
+++ reklama/urls.py
@@ -23,7 +23,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("app_reklama.urls"))
+    path("", include("app_reklama.urls")),
+    path("myauth/", include("app_auth.urls"))
 ]
 
 if settings.DEBUG:
